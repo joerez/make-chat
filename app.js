aa11b6c80cf55ca772ae87e326f4c36b36537c4e
Joerez <rezenbiz@gmail.com>
2018-09-11 17:03:44 -0700

    great

---
+++ app.js
@@ -0,0 +1,30 @@
+//App.js
+const express = require('express');
+const app = express();
+//Socket.io has to use the http server
+const server = require('http').Server(app);
+
+//Express View Engine for Handlebars
+const exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+//establish your public folder
+app.use('/public', express.static('public'))
+
+//Socket.io stuff
+const io = require('socket.io')(server);
+let onlineUsers = {};
+//Save the channels in this object.
+let channels = {"General" : []}
+io.on("connection", (socket) => {
+  // Make sure to send the channels to our chat file
+  require('./sockets/chat.js')(io, socket, onlineUsers, channels);
+})
+
+app.get('/', (req, res) => {
+  res.render('index.handlebars');
+})
+
+server.listen('3000', () => {
+  console.log('Server listening on Port 3000');
+})
